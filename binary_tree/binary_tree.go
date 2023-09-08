884a43fe0213df5ffe858d168553bf34aaf99440
Razorfang <jamiegetty1@gmail.com>
2023-09-08 22:31:07 +1200

    I'll figure out pathing tomorrow

--- binary_tree/binary_tree.go
+++ binary_tree/binary_tree.go
@@ -1,2 +1,30 @@
-package binary_tree
+package special_binary_tree
 
+import (
+	"fmt"
+)
+
+type Tree[T any] struct {
+	root *TreeNode[T]
+	nodeCount uint64
+}
+
+type TreeNode[T any] struct {
+	value T
+	leftNode *TreeNode[T]
+	rightNode *TreeNode[T]
+}
+
+/* In order for an element to exist in a binary tree, we must be able to compare it to another element */
+type TreeElement[T any] interface {
+	compare(a TreeNode[T], b TreeNode[T]) int
+}
+
+func newTree[T any]() *Tree[T] {
+	return &Tree[T]{nil, 0}
+}
+
+func test() {
+	tree := newTree[float64]()
+	fmt.Println(tree.nodeCount, tree.root)
+}
